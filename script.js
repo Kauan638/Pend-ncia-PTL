c4ce68e210010539f04f8c242d50fe00e530f934
Braga <jutsubra@gmail.com>
2026-06-20 00:52:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -939,3 +939,15 @@ ${document.getElementById(
     });
 
 }
+
+function copiarWhatsappCompleto(){
+    alert("Completo OK");
+}
+
+function copiarWhatsappResumo(){
+    alert("Resumo OK");
+}
+
+function baixarImagemResumo(){
+    alert("Imagem OK");
+}
